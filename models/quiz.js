505449c88ad1c2cefbf64a21630a596b05818dc2
franciscojose10 <franciscojose10@gmail.com>
2015-06-08 03:40:49 -0700

    Índice Temático

--- models/quiz.js
+++ models/quiz.js
@@ -9,6 +9,11 @@ module.exports = function(sequelize, DataTypes) {
       respuesta: {
         type: DataTypes.STRING,
         validate: { notEmpty: {msg: "-> Falta Respuesta"}}
+      },
+      indice: {
+        type: DataTypes.ENUM,
+        values: ["otro", "humanidades", "ocio", "ciencia", "tecnologia"],
+        validate: { notEmpty: {msg: "-> Falta Tema"}}
       }
     }
   );
